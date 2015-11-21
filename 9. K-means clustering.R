04d1854cfa32f4ba602c3d890c6b6065836e52cb
Richard <richarddeng88@gmail.com>
2015-11-21 15:57:21 -0500

    11 21

--- 9. K-means clustering.R
+++ 9. K-means clustering.R
@@ -6,23 +6,23 @@ sapply(teens, function(x){sum(is.na(x))})
 table(teens$gender, useNA = "ifany")
 
 ## DATA PREPROCESSIN - dummy coding missing values
-summary(teens$age)
-teens$age <- ifelse(teens$age >= 13 & teens$age < 20, teens$age, NA)
-summary(teens$age)
-
-teens$female <- ifelse(teens$gender == "F" & !is.na(teens$gender), 1, 0)
-teens$no_gender <- ifelse(is.na(teens$gender), 1, 0)
-
-table(teens$gender, useNA = "ifany")
-table(teens$female, useNA = "ifany")
-table(teens$no_gender, useNA = "ifany")
+    summary(teens$age)
+    teens$age <- ifelse(teens$age >= 13 & teens$age < 20, teens$age, NA)
+    summary(teens$age)
+    
+    teens$female <- ifelse(teens$gender == "F" & !is.na(teens$gender), 1, 0)
+    teens$no_gender <- ifelse(is.na(teens$gender), 1, 0)
+    
+    table(teens$gender, useNA = "ifany")
+    table(teens$female, useNA = "ifany")
+    table(teens$no_gender, useNA = "ifany")
 
 ## TRAIN MODEL
-interests <- teens[5:40]
-interests_z <- as.data.frame(lapply(interests, scale))
-
-set.seed(2345)
-teen_clusters <- kmeans(interests_z, 5)
+    interests <- teens[5:40]
+    interests_z <- as.data.frame(lapply(interests, scale))
+    
+    set.seed(2345)
+    teen_clusters <- kmeans(interests_z, 5)
     
 ## EVALUATING THE PERFORMANCE
 teen_clusters$size
