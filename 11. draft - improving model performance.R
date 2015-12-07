683ab2d0664dff47649cf3d860a223e551d83b08
Richard <richarddeng88@gmail.com>
2015-12-07 15:04:56 -0500

    1

--- 11. draft - improving model performance.R
+++ 11. draft - improving model performance.R
@@ -1,3 +1,11 @@
+## you already have a model, but every model has its own parameters setting. 
+## How to automate model performance tuning by searching for the optimal set of training conditions?
+library(caret)
+modelLookup("C5.0")  # use modelLookup() function to find the tuning parameters. 
+modelLookup("rpart")
+modelLookup("glm")
+
+
 credit <- read.csv("data/Machine-Learning-with-R-datasets/credit.csv")
 credit$default <- factor(credit$default, levels = c(1,2), labels = c("yes", "no"))
 
